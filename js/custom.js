60e2884d35c3906b5358065e9de086f5643f9c37
Sam Messina <samuel.messina@gmail.com>
2016-11-12 16:52:37 -0600

    new project section

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,3 @@
-/*!
- * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
 // Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
@@ -26,19 +9,8 @@ $(function() {
     });
 });
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top'
-})
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
-
 
 // slide down on mail click
-// o
 $(function() {
     $('a.scroll-link').bind('click', function(event) {
         var $anchor = $(this);
@@ -48,3 +20,5 @@ $(function() {
         event.preventDefault();
     });
 });
+
+

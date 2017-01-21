9072bbdfbbe721e27c8571ff221972150610397d
Sam Messina <samuel.messina@gmail.com>
2017-01-21 11:12:00 -0600

    taking efforts to decrease page load time

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,21 +1,4 @@
 let styles = {
-    HeaderContainerStyle: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#f3ebda',
-        textAlign: 'center',
-        height: '100vh',
-        backgroundColor: "#703030",
-        backgroundSize: 'cover'
-    },
-    HeaderInnerStyle: {
-    },
-    ChevronStyle: {
-        position: 'absolute',
-        bottom: '20px',
-        animation: 'bounce 3s infinite'
-    },
     Center: {
         textAlign: 'center',
     },
